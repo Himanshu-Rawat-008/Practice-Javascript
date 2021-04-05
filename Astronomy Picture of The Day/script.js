cf8d8e34405574b06bb012f377a842c1e9e83bfe
Himanshu <himanshurawat008.hr@gmail.com>
2021-04-05 16:37:18 +0530

    faster way to do

--- Astronomy Picture of The Day/script.js
+++ Astronomy Picture of The Day/script.js
@@ -1,11 +1,18 @@
 
 function displayImage(data){
-	console.log(data);
-	$('<img>',{
-		src: data.url,
-		width:'100%',
-		height:'100%',
-	}).appendTo('#image-container');
+	
+	// This method take time 
+
+	// console.log(data);
+	// $('<img>',{
+	// 	src: data.url,
+	// 	width:'100%',
+	// 	height:'100%',
+	// }).appendTo('#image-container');
+
+	 var img = $(document.createElement('img'));
+	 img.attr('src', data.url);
+	 img.appendTo('#image-container');
 }
 $.ajax({
 	// url:"https://api.nasa.gov/planetary/apod?api_key=vaEsaPmOKEQdwiVDjcCpZfJHdbZNqRZFVTV4JNQ2&date=2020-07-25",
